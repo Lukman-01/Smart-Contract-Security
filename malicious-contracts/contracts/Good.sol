3ac50b5a3e32fd4bd3a96a1e06438ea27b6a20d7
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-10-07 12:09:01 +0100

    updated

--- malicious-contracts/contracts/Good.sol
+++ malicious-contracts/contracts/Good.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.18;
 import "./Helper.sol";
 
+// Create a new contract, instead of typecasting an address 
+// into a contract inside the constructor. So instead of doing Helper(_helper) 
+// where you are typecasting _helper address into a contract which may or may not 
+// be the Helper contract, create an explicit new helper contract instance using new Helper().
 contract Good {
-    Helper helper;
-    constructor(address _helper) payable {
-        helper = Helper(_helper);
+    Helper public helper;
+    constructor() payable {
+        helper = new Helper();
     }
 
     function isUserEligible() public view returns(bool) {
